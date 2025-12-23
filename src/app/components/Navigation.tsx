f98fdfdc3e03847ff9ed46377d5cb5d38ce45ce4
Vijey-3 <ksvijey13@gmail.com>
2025-12-23 21:17:50 +0530

    Image Width Removed

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -12,7 +12,7 @@ export function Navigation() {
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center justify-center">
           <div className="w-[50%]">
-            <img src="Logo.jpg" className="h-32 w-full pt-4" />
+            <img src="Logo.jpg" className="w-full pt-4" />
           </div>
             <span className="text-2xl font-semibold bg-gradient-to-r from-purple-600 to-pink-600 dark:from-purple-400 dark:to-pink-400 bg-clip-text text-transparent w-[50%]">
               Feelix
