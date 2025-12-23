c0729026bf1cbeb12a09e47e15e7688a3d1762d3
Vijey-3 <ksvijey13@gmail.com>
2025-12-23 21:10:18 +0530

    Box Changed

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -11,10 +11,10 @@ export function Navigation() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center justify-center">
-          <div className="w-[30%]">
+          <div className="w-[40%]">
             <img src="Logo.jpg" className="h-32 w-full pt-4" />
           </div>
-            <span className="text-2xl font-semibold bg-gradient-to-r from-purple-600 to-pink-600 dark:from-purple-400 dark:to-pink-400 bg-clip-text text-transparent w-[70%]">
+            <span className="text-2xl font-semibold bg-gradient-to-r from-purple-600 to-pink-600 dark:from-purple-400 dark:to-pink-400 bg-clip-text text-transparent w-[60%]">
               Feelix
             </span>
           </Link>
