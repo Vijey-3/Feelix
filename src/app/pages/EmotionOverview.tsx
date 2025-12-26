f3c4e70631a86b6c3f57efb88877dce774a54de1
Vijey-3 <ksvijey13@gmail.com>
2025-12-26 23:57:35 +0530

    Styles Changed

--- src/app/pages/EmotionOverview.tsx
+++ src/app/pages/EmotionOverview.tsx
@@ -132,7 +132,7 @@ export function EmotionOverview() {
                 </Button>
               </Link>
               <Link to="/tools">
-                <Button variant="outline" size="lg" className="border-white text-white hover:bg-white/20">
+                <Button variant="outline" size="lg" className="bg-white text-purple-600 hover:bg-gray-100">
                   Explore Tools
                 </Button>
               </Link>
